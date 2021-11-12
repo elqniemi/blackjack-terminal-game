5ad602690e8189fa1d82d53b9969ec1103ed2ea3
Elmeri Niemi <elmeri.niemi@gmail.com>
2021-11-12 21:58:15 +0800

    Edited classes for multiplayer capabilities

--- blackjack.py
+++ blackjack.py
@@ -82,6 +82,9 @@ class Player:
         self.amount_bet = amount_bet
         self.money = self.money - amount_bet
 
+    def name(self):
+        return str(self.name())
+
 
 
 class Dealer(Player):
@@ -117,71 +120,46 @@ class Dealer(Player):
 
 ######### Gameplay ############
 
-own_name = str(input("Enter your name: "))
-amt_of_players = int(input(f"Hello {own_name}.\nPlease specify amount of players in game (1 - 6): "))
-player_name = str()
+amt_of_players = int(input(f"Hello! \nPlease specify amount of players in game (1 - 6): "))
+player_names = []
+
+for player in range(amt_of_players):
+    print(f"Input Player {player + 1} money and name.")
+    playerobj = Player(input("Amount of money:"), input("Player name:"))
+    player_names.append(playerobj)
+
 playing_cards = blackjack_cards(int(input('How many decks? ')), deck)
-total_money = int(input("How much $$$ you have?"))
-player_names = [own_name, "Player2", "Player3", "Player4", "Player5", "Player6"]
+
 dealer = Dealer()
 
 if amt_of_players == 1:
     print("Playing alone")
-    player_names = [player_names.pop(i) for i in range(amt_of_players)]
+
 elif amt_of_players in range(2, 7):
-    print(f"Playing with {amt_of_players - 1} other persons.")
-    player_names = [player_names.pop(0) for i in range(amt_of_players)]
+    print(f"Playing with {amt_of_players}  persons.")
+
 else:
     error_test = int(input('Invalid number of players. Press 1 to restart, 2 to play alone.'))
     if error_test == 2:
         amt_of_players = 1
-        player_names = [player_names.pop(i) for i in range(amt_of_players)]
+
         print("Playing alone")
     else:
         raise Exception("Something was incorrectly entered, restart")
 
-player_names_as_str = str()
-for i in range(len(player_names)):
-    assert isinstance(i, object)
-    player_names_as_str += (player_names[i] + ", ")
-
-print(f'Dealer: We have {player_names_as_str}playing now.')
-
-
-# bet = int(input('Bet: '))
-
-# Player objects for gameplay
-
-players = []
-player_1 = Player(total_money, own_name)
-
-if amt_of_players >= 2:
-    player_2 = Player(9999, player_names[1])
-
-if amt_of_players >= 3:
-    player_3 = Player(9999, player_names[2])
-
-if amt_of_players >= 4:
-    player_4 = Player(9999, player_names[3])
-
-if amt_of_players >= 5:
-    player_5 = Player(9999, player_names[4])
-
-if amt_of_players > 5:
-    player_6 = Player(9999, player_names[5])
 
 
 def blackjack(sum_cards):
     if sum_cards == 21:
         print('Blackjack!')
 
-def round_of_blackjack(player):
+def round_of_blackjack(players):
     print('Dealer: Place your bets!')
-    player_1.bet(int(input('Bet amount: ')))
+    # player_1.bet(int(input('Bet amount: ')))
     input("Press any key to deal...")
 
     round_dealer = dealer.deal(playing_cards)
-    round_player = player.deal(playing_cards)
+    round_player = [player.deal(playing_cards) for player in players]
 
     dealer_cards_value = round_dealer[0][1] + round_dealer[1][1]
     amtcards = 2
@@ -190,9 +168,11 @@ def round_of_blackjack(player):
         dealer_cards_value += round_dealer[amtcards][1]
         amtcards += 1
 
+    # for player in players
 
 
 
 
 
-round_of_blackjack(player_1)
+
+round_of_blackjack(player_names)
